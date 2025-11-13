a359a84230453a9f2fedcf43bd435b9f9b054944
iamza1d <zaidsafwah1@gmail.com>
2025-11-13 12:21:46 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -8,7 +8,7 @@ var games = [
     id: 'funfri',
     title: 'FunFri Games',
     url: 'https://funfrinew.neocities.org/',
-    image: 'https://i.ibb.co/TDbnBW9D/favicon.jpg'
+    image: 'https://i.ibb.co/qLPTwBV7/favicon.jpg'
   },
   {
     id: 'crazygames',
